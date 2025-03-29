a6fddd20fb357660153257f92840425ec7e4db63
VerbaOculta <faraya@gmail.com>
2025-03-29 20:57:50 -0300

    Update predict.js

--- api/predict.js
+++ api/predict.js
@@ -54,7 +54,6 @@ export default async function handler(req, res) {
       max_tokens: 1000
     });
 
-    console.error('Prompt', prompt);
     const response = completion.choices[0].message.content;
     res.status(200).json({ prediction: response });
 
