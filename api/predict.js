7b7f67183657e012ec370b6ab05dfab550a1b919
VerbaOculta <faraya@gmail.com>
2025-03-29 20:38:37 -0300

    Update predict.js

--- api/predict.js
+++ api/predict.js
@@ -55,7 +55,7 @@ export default async function handler(req, res) {
     
     });
 
-    console.error('Promot', completion.choices[0].message.content);
+    console.error('Promot', prompt);
     const response = completion.choices[0].message.content;
     res.status(200).json({ prediction: response });
   } catch (error) {
